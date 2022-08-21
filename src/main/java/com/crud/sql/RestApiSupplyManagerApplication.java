115109a4a0c86ee2b2deaeeebac4356df62eb6e9
josepmartorell <martorelljosep>
2022-08-21 18:46:48 +0200

    start project

---
+++ src/main/java/com/crud/sql/RestApiSupplyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.crud.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSupplyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSupplyManagerApplication.class, args);
+	}
+
+}
